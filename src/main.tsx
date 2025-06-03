e25831726a07767d2b163110c5ab1d2743e680aa
gegexb <georgesbonamy@hotmail.com>
2025-06-03 11:42:02 +0400

    fix: resolved all remaining main.tsx issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -20,4 +20,14 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
->>>>>>> 00b6d8d (Initial commit)
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
